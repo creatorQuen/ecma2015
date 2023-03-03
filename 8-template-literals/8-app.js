4affad6c595e3aa0959924f1d61b36420617525f
creatorQuen <spastime@yandex.ru>
2023-03-03 09:02:52 +0300

    material: template literals

---
+++ 8-template-literals/8-app.js
@@ -0,0 +1,14 @@
+const user = 'Sam';
+const num = 23;
+const text = `Hello ${user}, you old ${num + 10}.`
+
+console.log(text,'\n');
+
+
+//--------------------------------
+const html = `
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+    </ul>
+`;
